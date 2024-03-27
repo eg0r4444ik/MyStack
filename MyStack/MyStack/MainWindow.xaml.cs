42824b5242c6e6d48e91e812121efb45acad4f98
Rogachev Egor <55750592+eg0r4444ik@users.noreply.github.com>
2024-03-27 23:58:35 +0300

    Add files via upload

---
+++ MyStack/MyStack/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyStack
+{
+    public partial class MainWindow : Window
+    {
+        private MyStack<int> myStack;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            myStack = new MyStack<int>();
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            txtCurrentElement.Text = myStack.IsEmpty ? "N/A" : myStack.CurrentElement.ToString();
+            txtCount.Text = myStack.Count.ToString();
+            txtIsEmpty.Text = myStack.IsEmpty.ToString();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            int newItem;
+            if (int.TryParse(txtInput.Text, out newItem))
+            {
+                myStack.Add(newItem);
+                UpdateDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное число типа integer");
+            }
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                myStack.Remove();
+                UpdateDisplay();
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            myStack.Clear();
+            UpdateDisplay();
+        }
+    }
+}
---
+++ MyStack/MyStack/MyStack.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStack
+{
+    internal class MyStack<T>
+    {
+        private class Node
+        {
+            public T Data { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T data)
+            {
+                Data = data;
+                Next = null;
+            }
+        }
+
+        private Node last;
+        private int count;
+
+        public MyStack()
+        {
+            last = null;
+            count = 0;
+        }
+
+        public T CurrentElement
+        {
+            get
+            {
+                if (IsEmpty)
+                    throw new InvalidOperationException("Стек пуст");
+                return last.Data;
+            }
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return count == 0; }
+        }
+
+        public void Add(T item)
+        {
+            Node newNode = new Node(item);
+            if (last == null)
+            {
+                last = newNode;
+            }
+            else
+            {
+                Node old = last;
+                newNode.Next = old;
+                last = newNode;
+            }
+            count++;
+        }
+
+        public T Remove()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Стек пуст");
+
+            T data = last.Data;
+            last = last.Next;
+            count--;
+            return data;
+        }
+
+        public void Clear()
+        {
+            last = null;
+            count = 0;
+        }
+    }
+}
